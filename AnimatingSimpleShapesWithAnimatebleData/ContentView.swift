b4b4b1b4c42ac82eef468ca250464387aa3d1a45
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-07 12:34:37 +0200

    init

--- AnimatingSimpleShapesWithAnimatebleData/ContentView.swift
+++ AnimatingSimpleShapesWithAnimatebleData/ContentView.swift
@@ -8,9 +8,62 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var insetAmount: CGFloat = 50
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Trapezoid(insetAmount: insetAmount)
+                .frame(width: 200, height: 100)
+                .onTapGesture {
+                    withAnimation {
+                        self.insetAmount = CGFloat.random(in: 10...90)
+                    }
+            }
+            Trapezoid(insetAmount: insetAmount)
+                .frame(width: 200, height: 100)
+                .onTapGesture {
+                    withAnimation {
+                        self.insetAmount = CGFloat.random(in: 10...90)
+                    }
+            }
+            Trapezoid(insetAmount: insetAmount)
+                .frame(width: 200, height: 100)
+                .onTapGesture {
+                    withAnimation {
+                        self.insetAmount = CGFloat.random(in: 10...90)
+                    }
+            }
+            Trapezoid(insetAmount: insetAmount)
+                .frame(width: 200, height: 100)
+                .onTapGesture {
+                    withAnimation {
+                        self.insetAmount = CGFloat.random(in: 10...90)
+                    }
+            }
+        }
     }
 }
 
